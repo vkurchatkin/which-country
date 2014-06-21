644f05ba94b73218c8014cd43ed5c80454e0f1c9
Vladimir Kurchatkin <vladimir.kurchatkin@gmail.com>
2014-06-21 19:11:00 +0400

    use space delimited format for fixtures

--- test/index.js
+++ test/index.js
@@ -1,8 +1,17 @@
-var data = require('./data.json');
 var assert = require('assert');
+var fs = require('fs');
 
 var country = require('../index.js');
+var data = fs.readFileSync(__dirname + '/data.txt', 'utf8').split('\n');
 
-data.forEach(function (item) {
-  assert.equal(country(item.point), item.country);
+data.forEach(function (line) {
+  var parts = line.split(' ');
+  var id = parts[0];
+
+  for (var i = 1; i < parts.length; i++) {
+    var part = parts[i];
+    var coords = part.split(',').map(Number);
+
+    assert.equal(country([coords[1], coords[0]]), id);
+  }
 });
